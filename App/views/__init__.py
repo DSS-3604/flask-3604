f2efd18bf9f6b7136d1a056aca5158157cba1afc
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-02-26 22:40:47 -0400

    Posts Model, Controller, Route

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,4 +1,5 @@
 from .user import user_views
 from .index import index_views
+from .posts import posts_views
 
-views = [user_views, index_views]
+views = [user_views, index_views, posts_views]
---
+++ App/views/posts.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt import jwt_required
+
+
+from App.controllers import (
+    get_user,
+    create_post,
+    get_all_posts,
+    get_all_posts_json,
+)
+
+posts_views = Blueprint("posts_views", __name__, template_folder="../templates")
+
+# Get posts
+@posts_views.route("/api/posts", methods=["GET"])
+def get_posts_action():
+    posts = get_all_posts()
+    if posts:
+        return jsonify(get_all_posts_json()), 200
+    else:
+        return jsonify({"message": "No Posts exist"}), 404
