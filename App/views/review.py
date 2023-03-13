6f05ebf97d0edad105a5b53746c6ed4064894dc1
Daniel Yatali <danielyatali902@gmail.com>
2023-03-13 17:57:06 -0400

    changed route names for reviews so they are specific to products

--- App/views/review.py
+++ App/views/review.py
@@ -17,30 +17,34 @@ from App.controllers.review import (
     update_review,
     delete_review,
 )
-
-from App.controllers.user import is_farmer, is_admin
+from flask_jwt import jwt_required, current_identity
 
 review_views = Blueprint("review_views", __name__, template_folder="../templates")
 
-@review_views.route("/review", methods=["GET"])
+
+@review_views.route("/product/reviews", methods=["GET"])
+@jwt_required()
 def get_all_reviews_action():
     reviews = get_all_reviews_json()
     if reviews:
         return jsonify(reviews), 200
-    return jsonify({"message": "No products found"}), 404
+    return jsonify([]), 200
 
-@review_views.route("/review", methods=["POST"])
+@review_views.route("/product/review", methods=["POST"])
+@jwt_required()
 def create_review_action():
     data = request.json
 
-    create_review(     
-        product_id=data["product_id"], 
-        user_id=data["user_id"], 
+    create_review(
+        product_id=data["product_id"],
+        user_id=data["user_id"],
         body=data["body"]
     )
     return jsonify({"message": f"Review {data['product_id']} created"}), 201
 
-@review_views.route("/review/<int:id>", methods=["PUT"])
+
+@review_views.route("/product/review/<int:id>", methods=["PUT"])
+@jwt_required()
 def update_review_action(id):
     data = request.json
     review = get_review_by_id(id)
@@ -50,11 +54,12 @@ def update_review_action(id):
         return jsonify({"message": f"Review {id} updated"}), 200
     return jsonify({"message": "No review found"}), 404
 
-@review_views.route("/review/<int:id>", methods=["DELETE"])
+
+@review_views.route("/product/review/<int:id>", methods=["DELETE"])
+@jwt_required()
 def delete_review_action(id):
     review = get_review_by_id(id)
     if review:
         delete_review(id)
         return jsonify({"message": f"Review {id} deleted"}), 200
     return jsonify({"message": "No review found"}), 404
-
