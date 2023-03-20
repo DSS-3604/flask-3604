38a31a419867554b4f39902ba4f49c624d11f7d1
satyaandm <satyaandm@gmail.com>
2023-03-20 15:40:06 -0400

    added user name to p_comment.to_json

--- App/models/p_comment.py
+++ App/models/p_comment.py
@@ -1,5 +1,6 @@
 from App.database import db
 import datetime
+from App.models.user import User
 
 
 class ProductComment(db.Model):
@@ -27,6 +28,7 @@ class ProductComment(db.Model):
             "id": self.id,
             "product_id": self.product_id,
             "user_id": self.user_id,
+            "user_name": User.query.filter_by(id=self.user_id).first().username,
             "body": self.body,
             "timestamp": self.timestamp,
         }
