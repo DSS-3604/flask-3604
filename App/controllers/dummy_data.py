a01a9d4ad9e76ea7f06970901882628d2b4dc7f1
satyaandm <satyaandm@gmail.com>
2023-04-04 10:15:24 -0400

    clean up imports

--- App/controllers/dummy_data.py
+++ App/controllers/dummy_data.py
@@ -1,12 +1,7 @@
-from App.models import *
 from App.controllers.user import *
 from App.controllers.farmer_application import *
 from App.controllers.product import *
 from App.controllers.product_category import *
-from App.controllers.farmer_review import *
-from App.controllers.p_comment import *
-from App.controllers.p_reply import *
-from App.database import db
 import random
 
 # create 10 dummy regular users
