fb6b195cd735347f21a041071b04285bd9411523
satyaandm <satyaandm@gmail.com>
2023-04-05 20:34:00 -0400

    add applicant's username to constructor and model instead of querying in tojson

--- App/models/farmer_application.py
+++ App/models/farmer_application.py
@@ -7,6 +7,7 @@ class FarmerApplication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(120), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_name = db.Column(db.String(120), nullable=True)
     comment = db.Column(db.String(1024), nullable=True)
     created_timestamp = db.Column(db.DateTime, nullable=False)
     updated_timestamp = db.Column(db.DateTime, nullable=False)
@@ -14,9 +15,11 @@ class FarmerApplication(db.Model):
     def __init__(
         self,
         user_id,
+        user_name,
         comment="",
     ):
         self.user_id = user_id
+        self.user_name = user_name
         self.status = "Pending"
         self.comment = comment
         self.created_timestamp = datetime.now()
@@ -26,7 +29,7 @@ class FarmerApplication(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "user_name": User.query.filter_by(id=self.user_id).first().username,
+            "user_name": self.user_name,
             "status": self.status,
             "comment": self.comment,
             "created_timestamp": self.created_timestamp,
