9b4361b2a44020e4264cf9c90642fe26061a47b5
satyaandm <satyaandm@gmail.com>
2023-04-05 20:34:41 -0400

    add user_name to constructor and model instead of querying in tojson

--- App/models/p_comment.py
+++ App/models/p_comment.py
@@ -11,6 +11,7 @@ class ProductComment(db.Model):
     user_id = db.Column(
         db.Integer, db.ForeignKey("user.id"), nullable=False
     )  # foreign key links to user.id in user table
+    user_name = db.Column(db.String(120), nullable=True)
     body = db.Column(db.String(1024), nullable=False)  # body of comment
     timestamp = db.Column(db.DateTime, nullable=False)  # timestamp of comment
     updated_timestamp = db.Column(db.DateTime, nullable=False)
@@ -21,9 +22,10 @@ class ProductComment(db.Model):
         cascade="all, delete-orphan",
     )  # replies to comment
 
-    def __init__(self, product_id, user_id, body):
+    def __init__(self, product_id, user_id, user_name, body):
         self.product_id = product_id
         self.user_id = user_id
+        self.user_name = user_name
         self.body = body
         self.timestamp = datetime.now()
         self.updated_timestamp = datetime.now()
@@ -33,7 +35,7 @@ class ProductComment(db.Model):
             "id": self.id,
             "product_id": self.product_id,
             "user_id": self.user_id,
-            "user_name": User.query.filter_by(id=self.user_id).first().username,
+            "user_name": self.user_name,
             "body": self.body,
             "timestamp": self.timestamp,
             "updated_timestamp": self.updated_timestamp,
--- App/models/p_reply.py
+++ App/models/p_reply.py
@@ -13,13 +13,15 @@ class ProductReply(db.Model):
     user_id = db.Column(
         db.Integer, db.ForeignKey("user.id"), nullable=False
     )  # foreign key links to user.id in user table
+    user_name = db.Column(db.String(120), nullable=True)
     body = db.Column(db.String(1024), nullable=False)  # body of reply
     timestamp = db.Column(db.DateTime, nullable=False)  # timestamp of reply
     updated_timestamp = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, p_comment_id, user_id, body):
+    def __init__(self, p_comment_id, user_id, user_name, body):
         self.p_comment_id = p_comment_id
         self.user_id = user_id
+        self.user_name = user_name
         self.body = body
         self.timestamp = datetime.now()
         self.updated_timestamp = datetime.now()
@@ -29,7 +31,7 @@ class ProductReply(db.Model):
             "id": self.id,
             "p_comment_id": self.p_comment_id,
             "user_id": self.user_id,
-            "user_name": User.query.filter_by(id=self.user_id).first().username,
+            "user_name": self.user_name,
             "body": self.body,
             "timestamp": self.timestamp,
             "updated_timestamp": self.updated_timestamp,
