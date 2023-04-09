7f9acfc80b987f7403600aea5af50e348c298702
satyaandm <satyaandm@gmail.com>
2023-04-09 18:47:18 -0400

    remove flask_login stuff

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -1,4 +1,3 @@
-import flask_login
 from flask_jwt import JWT
 from App.models import User
 from App.controllers import get_user_by_username
@@ -14,14 +13,6 @@ def authenticate(username, password):
 # Payload is a dictionary which is passed to the function by Flask JWT
 def identity(payload):
     return User.query.get(payload["identity"])
-
-
-def login_user(user, remember):
-    return flask_login.login_user(user, remember=remember)
-
-
-def logout_user():
-    flask_login.logout_user()
 
 
 def setup_jwt(app):
